879469438cee78390b2c5f626ef3fbfa8c40530d
K1 <lukaszszczurek02@gmail.com>
2023-12-30 01:43:21 +0100

    fix: reformat code

--- src/main/java/org/horus/services/Wall.java
+++ src/main/java/org/horus/services/Wall.java
@@ -3,98 +3,64 @@ package org.horus.services;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.horus.exceptions.NotFoundException;
+import java.util.stream.Stream;
 import org.horus.interfaces.Block;
 import org.horus.interfaces.CompositeBlock;
 import org.horus.interfaces.Structure;
 
 public class Wall implements Structure {
-  private List<Block> blocks;
+  private final List<Block> blocks;
 
   public Wall() {
     this.blocks = new ArrayList<>();
   }
 
+  // Metoda dodająca blok do struktury ściany
   public void addBlock(List<Block> block) {
     blocks.addAll(block);
   }
 
   @Override
   public Optional<Block> findBlockByColor(String color) {
-    for (Block block : blocks) {
-      if (block instanceof CompositeBlock) {
-        Optional<Block> foundBlock = findBlockByColor((CompositeBlock) block, color);
-        if (foundBlock.isPresent()) {
-          return foundBlock;
-        }
-      } else if (block.getColor().equals(color)) {
-        return Optional.of(block);
-      }
-    }
-    throw new NotFoundException("Block with color " + color + " not found");
-  }
-
-  private Optional<Block> findBlockByColor(CompositeBlock compositeBlock, String color) {
-    for (Block block : compositeBlock.getBlocks()) {
-      if (block instanceof CompositeBlock) {
-        Optional<Block> foundBlock = findBlockByColor((CompositeBlock) block, color);
-        if (foundBlock.isPresent()) {
-          return foundBlock;
-        }
-      } else if (block.getColor().equals(color)) {
-        return Optional.of(block);
-      }
-    }
-    return Optional.empty();
+    return blocks.stream()
+        .flatMap(block -> {
+          if (color.equals(block.getColor())) {
+            return Stream.of(block);
+          } else if (block instanceof CompositeBlock) {
+            return ((CompositeBlock) block).getBlocks().stream();
+          } else {
+            return Stream.empty();
+          }
+        })
+        .filter(block -> color.equals(block.getColor()))
+        .findFirst();
   }
 
   @Override
   public List<Block> findBlocksByMaterial(String material) {
-    List<Block> blocksByMaterial = new ArrayList<>();
-    for (Block block : blocks) {
-      if (block instanceof CompositeBlock) {
-        blocksByMaterial.addAll(findBlocksByMaterial((CompositeBlock) block, material));
-      } else if (block.getMaterial().equals(material)) {
-        blocksByMaterial.add(block);
-      }
-    }
-    return blocksByMaterial;
-  }
+    List<Block> result = new ArrayList<>();
 
-  private List<Block> findBlocksByMaterial(CompositeBlock compositeBlock, String material) {
-    List<Block> blocksByMaterial = new ArrayList<>();
-    for (Block block : compositeBlock.getBlocks()) {
-      if (block instanceof CompositeBlock) {
-        blocksByMaterial.addAll(findBlocksByMaterial((CompositeBlock) block, material));
-      } else if (block.getMaterial().equals(material)) {
-        blocksByMaterial.add(block);
-      }
-    }
-    return blocksByMaterial;
+    blocks.stream()
+        .flatMap(block -> {
+          if (material.equals(block.getMaterial())) {
+            return Stream.of(block);
+          } else if (block instanceof CompositeBlock) {
+            return ((CompositeBlock) block).getBlocks().stream();
+          } else {
+            return Stream.empty();
+          }
+        })
+        .filter(block -> material.equals(block.getMaterial()))
+        .forEach(result::add);
+    return result;
   }
 
   @Override
   public int count() {
-    int count = 0;
-    for (Block block : blocks) {
-      if (block instanceof CompositeBlock) {
-        count += count((CompositeBlock) block);
-      } else {
-        count++;
-      }
-    }
-    return count;
-  }
-
-  private int count(CompositeBlock compositeBlock) {
-    int count = 0;
-    for (Block block : compositeBlock.getBlocks()) {
-      if (block instanceof CompositeBlock) {
-        count += count((CompositeBlock) block);
-      } else {
-        count++;
-      }
-    }
-    return count;
+    return blocks.stream()
+        .mapToInt(block -> block instanceof CompositeBlock ?
+            ((CompositeBlock) block).getBlocks().size() :
+            1)
+        .sum();
   }
 }
